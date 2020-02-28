4612933a476f5c002d73e780c6689cbb066191ff
Fernando Torres <webmasterft@users.noreply.github.com>
2020-02-28 13:44:59 -0500

    App.js
    Fix this is not function error

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     };
 
     this.onSearchChange = this.onSearchChange.bind(this);
-    this.SearchSubmit = this.onSearchSubmit.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.searchJokes = this.searchJokes.bind(this);
   }
 
