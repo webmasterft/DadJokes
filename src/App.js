9ec5271362c2c956249d6c8bfae9b0f286c7a1be
Fernando Torres <webmasterft@users.noreply.github.com>
2020-03-02 13:30:29 -0500

    Styling
    Adding Boostrap styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Loader from "./components/Loader";
 import SearchForm from "./components/SearchForm";
+import "bootswatch/dist/flatly/bootstrap.min.css";
 import "./styles.css";
 
 class App extends Component {
@@ -55,17 +56,28 @@ class App extends Component {
 
   renderJokes() {
     return (
-      <ul>
-        {this.state.jokes.map(item => (
-          <li key={item.id}>{item.joke}</li>
-        ))}
-      </ul>
+      <div className="bs-component my-5">
+        <ul className="list-group">
+          {this.state.jokes.map((item, index) => (
+            <li
+              className={
+                "list-group-item d-flex justify-content-between align-items-center" +
+                (index % 2 === 0 ? " active" : "")
+              }
+              key={item.id}
+            >
+              {item.joke}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
   render() {
     return (
       <div>
-        <div className="container">
+        <div className="col-md-12">
+          <h2 className="text-center">Dad Jokes</h2>
           <SearchForm
             onFormSubmit={this.onSearchSubmit}
             onSearchValueChange={this.onSearchChange}
@@ -74,7 +86,6 @@ class App extends Component {
           />
 
           {this.state.isFetching ? <Loader /> : this.renderJokes()}
-          
         </div>
       </div>
     );
